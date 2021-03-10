6644d4a4c18460578353541282f8811c935539e1
Marian Stochanskyi <stochan.andr@gmail.com>
2021-03-11 00:38:43 +0200

    Moved all code to one files

--- linear_model_generation.py
+++
@@ -1,8 +0,0 @@
-import sklearn.datasets as sl
-import sklearn.linear_model as lm
-
-input_data = sl.fetch_california_housing(as_frame=True)
-
-learning_regression = lm.LinearRegression()
-learning_regression.fit(input_data.data, input_data.target)
-
